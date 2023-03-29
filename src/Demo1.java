bf094f350c7d5abc696188ebf15dda121a9379fe
Lpc <li15950368002@163.com>
2023-03-29 13:30:57 +0800

    2023 3.29 first commit

---
+++ src/Demo1.java
@@ -0,0 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * IntelliJ IDEA-2018 3.2.
+ *
+ * @ClassName: Demo1
+ * @Date: 2023/03/29/13:16
+ * @Author: 李鹏程
+ * @Description:
+ */
+public class Demo1 {
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getByName("192.168.137.1");
+        System.out.println(address);
+    }
+
+}
