f7e98e6f5b63e4fe6054c2a61c9a42d30b18002b
Lpc <li15950368002@163.com>
2023-03-29 14:04:31 +0800

    2023 3.29 second commit

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,11 +1,33 @@
+import java.io.IOException;
+import java.net.*;
+
 /**
  * IntelliJ IDEA-2018 3.2.
  *
  * @ClassName: Demo2
  * @Date: 2023/03/29/13:51
  * @Author: 李鹏程
- * @Description:
+ * @Description:DUP发送和接收数据
  */
 public class Demo2 {
+    public static void main(String[] args) throws IOException {
 
+
+        //1.创建DatagramSocket对象
+        //绑定端口
+        //空参：所有可用端口随机一个使用
+        DatagramSocket ds = new DatagramSocket();
+
+        //2.打包数据
+        String str = "hello world !";
+        byte[] bytes = str.getBytes();
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        int port = 10086;
+        DatagramPacket dp = new DatagramPacket(bytes,bytes.length,address,port);
+        //3.发送数据
+        ds.send(dp);
+
+        //4.释放资源
+        ds.close();
+    }
 }
