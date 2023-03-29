d0b95eb039068837689ea7120dd1ec9cab1dd7eb
Lpc <li15950368002@163.com>
2023-03-29 14:33:58 +0800

    2023 3.29 last commit

---
+++ src/work/SendWork.java
@@ -0,0 +1,40 @@
+package work;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+/**
+ * IntelliJ IDEA-2018 3.2.
+ *
+ * @ClassName: SendWork
+ * @Date: 2023/03/29/14:20
+ * @Author: 李鹏程
+ * @Description:
+ */
+public class SendWork {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket ds = new DatagramSocket();//随机端口
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("输入你要说的话：");
+            String str = sc.nextLine();
+            if (str.equals("886"))break;
+            byte[] bytes =  str.getBytes();
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            int port = 10086;
+
+            DatagramPacket dp = new DatagramPacket(bytes,bytes.length,address,port);
+
+            ds.send(dp);
+        }
+
+        ds.close();
+
+
+    }
+}
